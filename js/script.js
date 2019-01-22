bc08dec34d007d70b8a9bb9cc0504a77403b37f8
Margo <sfibj890@mail.ru>
2019-01-22 22:48:24 +0300

    JS: modal_feedback; add: close
    - теперь появляется окно Обратной связи при нажатии на кнопку;
    - добавлено закрытие по кнопке.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var link = document.querySelector(".feedback_btn");
 var popup = document.querySelector(".feedback_wrapper");
+var popup2 = document.querySelector(".modal_feedback");
+var close = popup.querySelector(".modal_close");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal_show");
+  popup2.classList.add("modal_show");
 });
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal_show");
+
+  });
